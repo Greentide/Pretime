886323c2aeddb0d25df94548f416405c77ce1e62
Greentide <dan.oak44@gmail.com>
2013-06-07 13:13:41 +0200

    Create preform.php

---
+++ preform.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+		<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Test Pre</title>
+		<link href="styles.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<form action="getpre.php" method="post">
+			Name:
+			<input name="fname" type="text" class="form" value='This.Means.War.2012.PL.iNT.DVDRiP.x264-N0L' size="115" />
+			<input name="Enter" type="submit" class="button" />
+		</form>
+	</body>
+</html>
